75b70e459d506e7d33b9a98a50d03997aef17137
yaroofie <yaroofie@gmail.com>
2022-12-17 18:38:44 +0330

    feat : Update layout and add navigation to it

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,11 @@
-export default function Layout ( { children } )
-{
-   return (
-      <div className="md:flex mt-4 max-w-4xl mx-auto gap-6 mb-24 md:mb-0">
-         {children}
+import NavigationCard from "./NavigationCard";
+export default function Layout({ children }) {
+  return (
+    <div className="md:flex mt-4 max-w-4xl mx-auto gap-6 mb-24 md:mb-0">
+      <div className="fixed md:static w-full bottom-0 md:w-3/12 -mb-5">
+        <NavigationCard />
       </div>
-   );
+      <div>{children}</div>
+    </div>
+  );
 }
