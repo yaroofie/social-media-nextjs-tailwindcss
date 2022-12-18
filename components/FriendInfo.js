6f1b1035a815638429a1af378bf2b63ccb91d39d
yaroofie <yaroofie@gmail.com>
2022-12-18 12:54:38 +0330

    fix : Fix friend info image url bug

--- components/FriendInfo.js
+++ components/FriendInfo.js
@@ -3,7 +3,7 @@ import Avatar from "./Avatar";
 export default function FriendInfo() {
   return (
     <div className="flex gap-2">
-      <Avatar />
+      <Avatar url="https://api.lorem.space/image/face?w=50&h=50&hash=z7n2b508"/>
       <div>
         <h3 className="font-bold text-xl">Jane Doe</h3>
         <div className="text-sm leading-3">5 mutual friends</div>
